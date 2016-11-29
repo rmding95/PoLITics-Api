52fd87c8a6f5fef8e5aeca4c560adc5bc1468f18
Richard Ding <richard.ding@alaskaair.com>
2016-11-28 20:33:10 -0800

    twitterscraper now inserts to db

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -5,6 +5,7 @@ import urllib
 import peewee
 from peewee import *
 
+
 def createQuery(file):
     query = 'q='
     first_line = file.readline().replace('#', '%23').strip('\n')
@@ -28,32 +29,39 @@ def getTweets(query, party):
             for state in mydict:
                 test1 = state[0] + ","
                 test2 = state[0] + " "
-                if test1.lower() in result.user.location.lower() or test2.lower() in result.user.location.lower() or state[2].lower() in result.user.location.lower():
+                if test1.lower() in result.user.location.lower() or test2.lower() in result.user.location.lower() or \
+                                state[2].lower() in result.user.location.lower():
                     stateid = state[0]
                     for city in mydict2[stateid]:
                         if city[0].lower() in result.user.location.lower():
-                            response = urllib.request.urlopen("http://api.zippopotam.us/us/" + city[1]).read().decode('utf-8')
+                            response = urllib.request.urlopen("http://api.zippopotam.us/us/" + city[1]).read().decode(
+                                'utf-8')
                             data = json.loads(response)
-                            if data.get('places')[0].get('latitude') == None or data.get('places')[0].get('longitude') == None:
+                            if data.get('places')[0].get('latitude') == None or data.get('places')[0].get(
+                                    'longitude') == None:
                                 break
-                            tweet = Tweet(data.get('places')[0].get('latitude'), data.get('places')[0].get('longitude'), party,result.created_at)
+                            tweet = Tweet(data.get('places')[0].get('latitude'), data.get('places')[0].get('longitude'),
+                                          party, result.created_at)
                             tweets.append(tweet)
                             locationsent = 1
                             break
                     if locationsent == 0:
                         for state in mydict:
                             if state[0] == stateid:
-                                response = urllib.request.urlopen("http://api.zippopotam.us/us/" + state[1]).read().decode('utf-8')
+                                response = urllib.request.urlopen(
+                                    "http://api.zippopotam.us/us/" + state[1]).read().decode('utf-8')
                                 data = json.loads(response)
-                                if data.get('places')[0].get('latitude') == None or data.get('places')[0].get('longitude') == None:
+                                if data.get('places')[0].get('latitude') == None or data.get('places')[0].get(
+                                        'longitude') == None:
                                     break
-                                tweet = Tweet(data.get('places')[0].get('latitude'), data.get('places')[0].get('longitude'), party, result.created_at)
+                                tweet = Tweet(data.get('places')[0].get('latitude'),
+                                              data.get('places')[0].get('longitude'), party, result.created_at)
                                 tweets.append(tweet)
                                 locationsent = 1
                                 break
 
                 if locationsent == 0:
-                    if result.user.location.lower() == "usa" or result.user.location.lower() == "us" or result.user.location.lower() == "united states" or result.user.location.lower() == "united states of america" :
+                    if result.user.location.lower() == "usa" or result.user.location.lower() == "us" or result.user.location.lower() == "united states" or result.user.location.lower() == "united states of america":
                         tweet = Tweet(34.024212, -118.496475, party, result.created_at)
                         tweets.append(tweet)
                         locationsent = 1
@@ -69,43 +77,62 @@ class Tweet:
         self.party = party
         self.timestamp = timestamp
 
+
 api = twitter.Api(consumer_key='rnBTENQ1GCJdZLVEuZheV6YJ6',
                   consumer_secret='b9g5TgNIXRKN7lwQxh5YcLk8AI59zQK3zzIAtAorspMHpUha3F',
                   access_token_key='787504691949076481-jwZbK3F3lc5evdzeExZO0DRj4LvWB1m',
                   access_token_secret='Q8DGppRwEFKWo5ZxbAjXCQUGAqBgCMU0t4ZI21RGoND3T')
 
 db = MySQLDatabase('politicsdb', host='politicsdb.cdcme9z9rkbx.us-west-2.rds.amazonaws.com',
-                   port=3306, user='richardding')
+                   port=3306, user='richardding', passwd='politics')
 db.connect()
 
-republican_file = open('../data/republican_hashtags.txt', 'r')
-democrat_file = open('../data/democrat_hashtags.txt', 'r')
+#db = MySQLDatabase('politicsdb')
+
+
+class Tweets(Model):
+    latitude = CharField()
+    longitude = CharField()
+    party = CharField()
+    timestamp = CharField()
+
+    class Meta:
+        database = db
+
+
+db.connect()
+
+Tweets.create_table()
+republican_file = open('data/republican_hashtags.txt', 'r')
+democrat_file = open('data/democrat_hashtags.txt', 'r')
 # q=%23Trump%2C%20OR%20%23Hillary%2C%20OR%20%23Kane&count=100
 republican_query = createQuery(republican_file)
 democrat_query = createQuery(democrat_file)
 
-with open('../data/StateZip.csv', mode='r') as infile:
+with open('data/StateZip.csv', mode='r') as infile:
     reader = csv.reader(infile)
     mydict = [[rows[0], rows[1], rows[2]] for rows in reader]
-
-
 
 mydict2 = {}
 for state in mydict:
     currentlist = []
-    with open('../data/Zipcodes.csv', mode='r') as infile:
+    with open('data/Zipcodes.csv', mode='r') as infile:
         reader = csv.reader(infile)
         for row in reader:
             if row[4] == state[0]:
-                currentlist.append([row[3],row[1]])
+                currentlist.append([row[3], row[1]])
         mydict2[state[0]] = currentlist
 
 tweets = []
 for i in range(0, 20):
     getTweets(republican_query, 'Republican')
     getTweets(democrat_query, 'Democrat')
-print(tweets)
+#print(tweets)
 
 for tweet in tweets:
+    record = Tweets(latitude=tweet.latitude, longitude=tweet.longitude, party=tweet.party, timestamp=tweet.timestamp)
+    record.save()
     print(tweet)
 
+for data in Tweets.select():
+    print(data.latitude + data.longitude + data.party + data.timestamp)
--- __init__.py
+++ __init__.py
@@ -1,16 +1,29 @@
-from flask import Flask
-#from flaskext.mysql import MySQL
+from flask import Flask, jsonify
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
-#mysql = MySQL()
-#app.config['MYSQL_DATABASE_HOST'] = 'politicsdb.cdcme9z9rkbx.us-west-2.rds.amazonaws.com'
-#mysql.init_app(app)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'richardding'
+app.config['MYSQL_DATABASE_HOST'] = 'politicsdb.cdcme9z9rkbx.us-west-2.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_DB'] = 'politicsdb'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'politics'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
-    #conn = mysql.connect()
-    #cursor = conn.cursor()
+
     return 'Hello World!'
+
+@app.route('/db')
+def connect():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    # query = """CREATE TABLE tweets (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    #  Latitude varchar(255), Longitude varchar(255), Party varchar(255), Timestamp varchar(255))"""
+    #cursor.execute(query)
+    query = "SELECT * FROM tweets"
+    cursor.execute(query)
+    return jsonify(data=cursor.fetchall())
 
 if __name__ == '__main__':
     app.run(debug=True)
